3f5101d7bc02c06b8f9624d5c0ce696df14276a5
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-12 20:08:49 +0530

    feat: add post routes

---
+++ Backend/router/postRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { createPost, editPost , deletePost,getAllPosts } from "../controllers/postController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import upload from "../middleware/uploadMiddleware.js";
+
+const router = express.Router();
+
+router.post("/create", protect, upload.single("image"), createPost);
+
+router.put("/edit/:postId", protect, editPost);
+router.delete("/delete/:postId", protect, deletePost);
+
+router.get("/", protect, getAllPosts);
+
+export default router;
