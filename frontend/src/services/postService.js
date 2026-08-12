6f598691876961d5a82d8ec285ef43bec7dbff3f
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-12 20:09:14 +0530

    feat: add post api services

---
+++ frontend/src/services/postService.js
@@ -0,0 +1,18 @@
+import api from "../services/api.js";
+export const createPost = async (formData) => {
+    const token = localStorage.getItem("token");
+
+    const response = await api.post("/posts/create", formData, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    return response.data;
+};
+
+export const getAllPosts = async (page = 1) => {
+    const response = await api.get(`/posts?page=${page}`);
+
+    return response.data;
+};
