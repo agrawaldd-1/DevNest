2496bd23bce701aee94d6a289eea7db74a798cc8
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-13 09:52:20 +0530

    feat: add project service

---
+++ frontend/src/services/projectService.js
@@ -0,0 +1,54 @@
+import api from "./api.js";
+
+export const createProject = async (formData) => {
+    const response = await api.post(
+        "/projects",
+        formData
+    );
+
+    return response.data;
+};
+
+export const getAllProjects = async (page = 1) => {
+    const response = await api.get(
+        `/projects?page=${page}`
+    );
+
+    return response.data;
+};
+
+export const viewProject = async (projectId) => {
+    const response = await api.get(
+        `/projects/${projectId}`
+    );
+
+    return response.data;
+};
+
+export const getUserProjects = async (userId) => {
+    const response = await api.get(
+        `/projects/user/${userId}`
+    );
+
+    return response.data;
+};
+
+export const editProject = async (
+    projectId,
+    projectData
+) => {
+    const response = await api.put(
+        `/projects/${projectId}`,
+        projectData
+    );
+
+    return response.data;
+};
+
+export const deleteProject = async (projectId) => {
+    const response = await api.delete(
+        `/projects/${projectId}`
+    );
+
+    return response.data;
+};
