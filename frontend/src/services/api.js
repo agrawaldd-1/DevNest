ce73d7f1addd6ff031bcb13dc4a5e4ad75c14b14
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-31 15:37:39 +0530

    pushed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://devnest-1-b73r.onrender.com/api",
 });
 
 api.interceptors.request.use(
